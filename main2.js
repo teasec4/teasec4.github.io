504b1c23f61b7af383cb656299831f14fdce9449
teasec4 <85108614+teasec4@users.noreply.github.com>
2021-12-18 11:23:09 +0800

    Add files via upload
    new color

--- main2.js
+++ main2.js
@@ -10,48 +10,48 @@ timer = setInterval(function () {
 
     if (document.getElementById("Choice1").checked === true){
         if(secondsIn >=1 && secondsIn<6){
-            timerIn.style.color = '#7abe7a'
+            timerIn.style.color = 'green'
             text.innerText = "inhale"
         }
         if (secondsIn >=6 && secondsIn<16){
-            timerIn.style.color = '#f83e3e'
+            timerIn.style.color = 'red'
             text.innerText = "exhale"
         }
         if (secondsIn >=16){
             secondsIn=1
-            timerIn.style.color = '#7abe7a'
+            timerIn.style.color = 'green'
             text.innerText = "inhale"
         }
     }
 
     if (document.getElementById("Choice2").checked === true){
         if(secondsIn >=1 && secondsIn<9){
-            timerIn.style.color = '#7abe7a'
+            timerIn.style.color = 'green'
             text.innerText = "inhale"
         }
         if (secondsIn >=9 && secondsIn<24){
-            timerIn.style.color = '#f83e3e'
+            timerIn.style.color = 'red'
             text.innerText = "exhale"
         }
         if (secondsIn >=24){
             secondsIn=1
-            timerIn.style.color = '#7abe7a'
+            timerIn.style.color = 'green'
             text.innerText = "inhale"
         }
     }
 
     if (document.getElementById("Choice3").checked === true){
         if(secondsIn >=1 && secondsIn<11){
-            timerIn.style.color = '#7abe7a'
+            timerIn.style.color = 'green'
             text.innerText = "inhale"
         }
         if (secondsIn >=11 && secondsIn<31){
-            timerIn.style.color = '#f83e3e'
+            timerIn.style.color = 'red'
             text.innerText = "exhale"
         }
         if (secondsIn >=31){
             secondsIn=1
-            timerIn.style.color = '#7abe7a'
+            timerIn.style.color = 'green'
             text.innerText = "inhale"
         }
     }
