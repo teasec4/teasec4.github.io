7b2b356b10c6c5675fac58faa4a5ccdc09bf707d
teasec4 <85108614+teasec4@users.noreply.github.com>
2021-12-30 19:38:31 +0800

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,85 @@
+let timerIn1 = document.getElementById("timerIn");
+let breath = document.getElementById("breathPlace")
+
+coloragaine = 'rgba(75,232,75,0.81)'
+coloranaine2 = 'rgba(241,20,47,0.85)'
+inhale = 'inhale'
+exhale = 'exhale'
+inhaleColor = '#F2E18D'
+exhaleColor = '#F2E18D'
+
+button.addEventListener('click', function () {
+    secondsIn = 1
+    timerIn1.style.color = 'white'
+})
+
+timer = setInterval(function () {
+
+    if (document.getElementById("Choice1").checked === true){
+        if(secondsIn >=1 && secondsIn<6){
+            timerIn1.style.backgroundColor = coloragaine
+            breath.innerHTML = inhale
+            breath.style.backgroundColor = inhaleColor
+
+        }
+        if (secondsIn >=6 && secondsIn<16){
+            timerIn1.style.backgroundColor = coloranaine2
+            breath.innerHTML = exhale
+            breath.style.backgroundColor = exhaleColor
+        }
+        if (secondsIn >=16){
+            secondsIn=1
+            timerIn1.style.backgroundColor = coloragaine
+            breath.innerHTML = inhale
+            breath.style.backgroundColor = inhaleColor
+        }
+    }
+
+    if (document.getElementById("Choice2").checked === true){
+        if(secondsIn >=1 && secondsIn<9){
+            timerIn1.style.backgroundColor = coloragaine
+            breath.innerHTML = inhale
+            breath.style.backgroundColor = inhaleColor
+        }
+        if (secondsIn >=9 && secondsIn<24){
+            timerIn1.style.backgroundColor = coloranaine2
+            breath.innerHTML = exhale
+            breath.style.backgroundColor = exhaleColor
+        }
+        if (secondsIn >=24){
+            secondsIn=1
+            timerIn1.style.backgroundColor = coloragaine
+            breath.innerHTML = inhale
+            breath.style.backgroundColor = inhaleColor
+        }
+    }
+
+    if (document.getElementById("Choice3").checked === true){
+        if(secondsIn >=1 && secondsIn<11){
+            timerIn1.style.backgroundColor = coloragaine
+            breath.innerHTML = inhale
+            breath.style.backgroundColor = inhaleColor
+        }
+        if (secondsIn >=11 && secondsIn<31){
+            timerIn1.style.backgroundColor = coloranaine2
+            breath.innerHTML = exhale
+            breath.style.backgroundColor = exhaleColor
+        }
+        if (secondsIn >=31){
+            secondsIn=1
+            timerIn1.style.backgroundColor = coloragaine
+            breath.innerHTML = inhale
+            breath.style.backgroundColor = inhaleColor
+        }
+    }
+
+    let strTimerIn = `${secondsIn}`;
+    timerIn.innerHTML = strTimerIn;
+
+    ++secondsIn;
+
+
+}, 1000);
+
+
+
